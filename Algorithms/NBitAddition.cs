71930d8cfc509edc5a525d253933946c5980bb09
Manpreet Rai <manpreetrai01@gmail.com>
2023-11-03 01:26:14 +0530

    Added Ex. 2.1-4

---
+++ Algorithms/NBitAddition.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    internal class NBitAddition
+    {
+        public int[] A {  get; set; } = new int[8]; // Size 8 bits
+        public int[] B { get; set; } = new int[8]; // Size 8 bits
+
+        public int[] C { get; set; } = new int[9]; // Size 9 bits, i.e., 8 for sum, 1 for carry
+
+        public NBitAddition() { }
+        public NBitAddition(int[] a, int[] b)
+        {
+            A = a;
+            B = b;
+        }
+
+        public int[] Sum()
+        {
+            int carry = 0;
+            for (int i = A.Length - 1; i >= 0; i--)
+            {
+                C[i + 1] = (A[i] + B[i] + carry) % 2;
+                if ((A[i] + B[i] + carry) >= 2)
+                    carry = 1;
+                else
+                    carry = 0;
+            }
+
+            C[0] = carry;
+
+            return C;
+        }
+    }
+}
--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -12,32 +12,51 @@ namespace Algorithms
             Console.Write("Current Sequence: ");
             PrintIntArray(sequence);
 
+
             // Algorithm 2.1
+            // Refer InsertionSort.cs
             InsertionSort insertionSort = new InsertionSort(sequence);
             int[] increasingSortedArray = insertionSort.IncreasingSort();
             Console.Write("Insertion Sorted Increasing Sequence: ");
             PrintIntArray(increasingSortedArray);
 
+
             // Question 2.1-1
+            // Refer InsertionSort.cs
             InsertionSort insertionSort2 = new InsertionSort(new int[] {31, 41, 59, 26, 41, 58});
             int[] increasingSortedArray2 = insertionSort2.IncreasingSort();
             Console.Write("Insertion Sorted Increasing Sequence on [31, 41, 59, 26, 41, 58]: ");
             PrintIntArray(increasingSortedArray2);
 
+
             // Question 2.1-2
+            // Refer InsertionSort.cs
             int[] decreasingSortedArray = insertionSort.DecreasingSort(); // Uses Algorithm 2.1 instance and sequence
             Console.Write("Insertion Sorted Decreasing Sequence: ");
             PrintIntArray(decreasingSortedArray);
 
-            //Question 2.1-3
+
+            // Question 2.1-3
+            // Refer LinearSearch.cs
             int[] searchSequence = new int[] { 31, 41, 59, 26, 41, 58 };
             LinearSearch linearSearch = new LinearSearch(searchSequence, 41);
             Console.WriteLine($"Index of {41} in [{string.Join(", ", searchSequence)}] is: {linearSearch.Search()}");
             linearSearch.SearchValue = 98;
             Console.WriteLine($"Index of {98} in [{string.Join(", ", searchSequence)}] is: {linearSearch.Search()}");
+            Console.WriteLine("**********************************");
 
 
-            //Helper Utility
+            // Question 2.1-4
+            // Refer NBitAddition.cs
+            int[] arrayA = new int[8] { 0, 1, 1, 0, 1, 1, 1, 1 };
+            int[] arrayB = new int[8] { 1, 1, 1, 0, 0, 0, 1, 1 };
+            NBitAddition nBitAddition = new NBitAddition(arrayA, arrayB);
+            Console.WriteLine($"Array A:    [ {string.Join(", ", arrayA)} ]");
+            Console.WriteLine($"Array B:    [ {string.Join(", ", arrayB)} ]");
+            Console.WriteLine($"Array C: [ {string.Join(", ", nBitAddition.Sum())} ]");
+
+
+            // Helper Utility
             static void PrintIntArray(int[] sequence)
             {
                 Console.WriteLine($"[ {string.Join(", ", sequence)} ]");
