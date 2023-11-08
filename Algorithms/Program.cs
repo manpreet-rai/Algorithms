6dd0480f5d867922504033425bd7e39968684f9e
Manpreet Rai <hi@manpreetrai.ca>
2023-11-08 18:09:33 +0530

    Added HeapSort

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -4,7 +4,7 @@ namespace Algorithms
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             Console.WriteLine("******* Welcome to Algorithms *******");
             
@@ -23,7 +23,7 @@ namespace Algorithms
             // Question 2.1-1
             // Refer InsertionSort.cs
             InsertionSort insertionSort2 = new InsertionSort();
-            int[] increasingSortedArray2 = insertionSort2.IncreasingSort(new int[] { 31, 41, 59, 26, 41, 58 });
+            int[] increasingSortedArray2 = insertionSort2.IncreasingSort(new[] { 31, 41, 59, 26, 41, 58 });
             PrintIntArray(increasingSortedArray2, "Insertion Sorted Increasing Sequence on [31, 41, 59, 26, 41, 58]: ", true);
 
 
@@ -35,7 +35,7 @@ namespace Algorithms
 
             // Question 2.1-3
             // Refer LinearSearch.cs
-            int[] searchSequence = new int[] { 31, 41, 59, 26, 41, 58 };
+            int[] searchSequence = { 31, 41, 59, 26, 41, 58 };
             LinearSearch linearSearch = new LinearSearch();
             Console.WriteLine($"Index of {41} in [{string.Join(", ", searchSequence)}] is: {linearSearch.Search(searchSequence, 41)}");
             Console.WriteLine($"Index of {98} in [{string.Join(", ", searchSequence)}] is: {linearSearch.Search(searchSequence, 98)}");
@@ -44,8 +44,8 @@ namespace Algorithms
 
             // Question 2.1-4
             // Refer NBitAddition.cs
-            int[] arrayA = new int[8] { 0, 1, 1, 0, 1, 1, 1, 1 };
-            int[] arrayB = new int[8] { 1, 1, 1, 0, 0, 0, 1, 1 };
+            int[] arrayA = { 0, 1, 1, 0, 1, 1, 1, 1 };
+            int[] arrayB = { 1, 1, 1, 0, 0, 0, 1, 1 };
             NBitAddition nBitAddition = new NBitAddition(arrayA, arrayB);
             PrintIntArray(arrayA, "Array A:    ");
             PrintIntArray(arrayB, "Array B:    ");
@@ -54,7 +54,7 @@ namespace Algorithms
 
             // Question 2.2-2
             // Refer SelectionSort.cs
-            sequence = new int[] { 41, 59, 26, 41, 58 };
+            sequence = new[] { 41, 59, 26, 41, 58 };
             PrintIntArray(sequence, "Current Sequence: ");
             SelectionSort selectionSort = new SelectionSort();
             PrintIntArray(selectionSort.Sort(sequence), "Selection Sorted Sequence: ", true);
@@ -62,7 +62,7 @@ namespace Algorithms
 
             // Question 2.3-1
             // Refer MergeSort.cs
-            sequence = new int[] { 3, 41, 52, 26, 38, 57, 9, 49 };
+            sequence = new[] { 3, 41, 52, 26, 38, 57, 9, 49 };
             PrintIntArray(sequence, "Current Sequence: ");
             MergeSort mergeSort = new MergeSort();
             PrintIntArray(mergeSort.Sort(sequence, 0, sequence.Length - 1), "Merge Sorted Sequence: ", true);
@@ -70,7 +70,7 @@ namespace Algorithms
 
             // Algorithm 2.3-5
             // Refer BinarySearch.cs
-            sequence = new int[] { 3, 9, 26, 38, 41, 49, 52, 57 };
+            sequence = new[] { 3, 9, 26, 38, 41, 49, 52, 57 };
             PrintIntArray(sequence, "Current Sequence: ");
             BinarySearch binarySearch = new BinarySearch();
             Console.WriteLine($"Index of {26} in [{string.Join(", ", sequence)}] is: {binarySearch.Search(sequence, 26, 0, sequence.Length - 1)}");
@@ -79,7 +79,7 @@ namespace Algorithms
 
             // Algorithm 2-2
             // Refer BubbleSort.cs
-            sequence = new int[] { 3, 41, 52, 26, 38, 57, 9, 49 };
+            sequence = new[] { 3, 41, 52, 26, 38, 57, 9, 49 };
             PrintIntArray(sequence, "Current Sequence: ");
             BubbleSort bubbleSort = new BubbleSort();
             PrintIntArray(bubbleSort.Sort(sequence), "Bubble Sorted Sequence: ", true);
@@ -87,12 +87,19 @@ namespace Algorithms
 
             // Algorithm 3.1
             // Refer MaxSubArray.cs
-            sequence = new int[] { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
+            sequence = new[] { 13, -3, -25, 20, -3, -16, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7 };
             PrintIntArray(sequence, "Current Sequence: ");
             MaxSubArray maxSubArray = new MaxSubArray();
             (int min, int max, int sum) = maxSubArray.FindMaxSubArray(sequence, 0, 15);
             PrintIntArray(sequence[min..(max + 1)], $"Max Sub Array with sum = {sum}: ", true);
 
+
+            // Algorithm 6.1
+            // Refer HeapSort.cs
+            sequence = new[] { 3, 41, 52, 26, 38, 57, 9, 49 };
+            PrintIntArray(sequence, "Current Sequence");
+            HeapSort heapSort = new HeapSort();
+            PrintIntArray(heapSort.Sort(sequence), "Max Heap Sorted: ", true);
 
             // Helper Utility
             static void PrintIntArray(int[] sequence, string message = "", bool pattern = false)
